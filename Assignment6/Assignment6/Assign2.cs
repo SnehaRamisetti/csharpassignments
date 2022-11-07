5f296e2f2713cece4ef7de13e157ef4bf60cb77a
SnehaRamisetti <115206730+SnehaRamisetti@users.noreply.github.com>
2022-11-07 10:06:40 +0530

    Add files via upload

---
+++ Assignment6/Assignment6/Assign2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using static Assignment6.Assign2;
+
+namespace Assignment6
+{
+    internal class Assign2
+    {
+        public delegate void sampledelegate();
+         public class Manager
+        {
+            double petrol;
+            double food;
+            double others;
+            double salary;
+
+            public void cal(double salary)
+            {
+                Console.WriteLine("salary:", salary);
+            }
+            public static void print1()
+            {
+
+                Console.WriteLine("petrol:");
+
+                Console.WriteLine("food:");
+
+                Console.WriteLine("others");
+            }
+
+        }
+        public class MarketingExecutive
+        {
+            double kilometer;
+            double tour;
+            double telephone;
+            double salary;
+
+
+            public static void print2()
+            {
+
+                Console.WriteLine("kilometer");
+
+                Console.WriteLine("TOUR:");
+
+                Console.WriteLine("TELEPHONE");
+            }
+
+        }
+        public class program
+        {
+            public static void Main()
+            {
+                Manager manager = new Manager();
+                Console.WriteLine("ENTER SALARY");
+                double salary = Convert.ToDouble(Console.ReadLine());
+                manager.cal(salary);
+                double petrol = salary * 8 / 100;
+                double food = salary * 13 / 100;
+                double others = salary * 3 / 100;
+                sampledelegate del1,del2,del3;
+                del1 = new sampledelegate(print1);
+                del2 = new sampledelegate(print2);
+                del3 = del1 + del2;
+                del3();
+                
+                
+               
+                MarketingExecutive marketingExecutive = new MarketingExecutive();
+                Console.WriteLine("enter kilometer");
+                double kilometer=Convert.ToDouble(Console.ReadLine());
+                double tour = kilometer * 5;
+                double telephone = 1000;
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Assignment6/Assignment6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public delegate void printdelegate(double salary,double petrol,double food,double others);
+    internal  class Manager
+    {
+        double petrol;
+        double food;
+        double others;
+        double salary;
+
+        public void cal(double salary)
+        {
+          Console.WriteLine("salary:",salary);
+        }
+        public  static void print(double salary, double petrol,double food,double others)
+        {
+            
+            Console.WriteLine("petrol:",petrol);
+          
+            Console.WriteLine("food:",food);
+
+            Console.WriteLine("others",others);
+        }
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            Console.WriteLine("ENTER SALARY");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            manager.cal(salary);
+           double petrol = salary * 8 / 100;
+           double food = salary * 13 / 100;
+           double others = salary * 3 / 100;
+            printdelegate del = new printdelegate(print);
+            del(salary,petrol,food,others);
+           
+            Console.ReadKey();
+        }
+        
+
+    }
+}
