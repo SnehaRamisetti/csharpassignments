ec05ccf9504af2a955e34e93159b76bddc2a2591
SnehaRamisetti <115206730+SnehaRamisetti@users.noreply.github.com>
2022-11-07 10:03:29 +0530

    Add files via upload

---
+++ Assign3B/Assign3B/Clone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign3B
+{
+    internal class Clone
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            
+
+            Stack mystack = new Stack(6);
+            mystack.Push(1);
+            mystack.Push(2);
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(5);
+           
+            mystack.Push(6);
+            mystack.Push(7);
+            foreach (int i in mystack)
+            {
+                Console.WriteLine(i);
+            }
+            Stack mystack2 = mystack;
+            foreach (int i in mystack2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Assign3B/Assign3B/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign3B
+{
+    internal class Mystack:ICloneable
+    {
+      
+        object ICloneable.Clone()
+        {
+            return Clone();
+            
+        }
+        public object Clone()
+        {
+            return new Mystack();
+            
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            int position;
+          
+            Stack<int> mystack = new Stack<int>(6);
+            
+           
+            mystack.Push(1);
+            mystack.Push(2);    
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(5);
+            mystack.Pop();
+            mystack.Push(6);
+           
+           
+            Console.WriteLine(mystack.Contains(7));
+           
+            foreach (int i in mystack)
+            {
+                Console.WriteLine(i);
+            }
+            position = mystack.Peek();
+            Console.WriteLine(position);
+            mystack.Clear();
+            Console.WriteLine("COUNT={0}",mystack.Count());
+           
+            
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
